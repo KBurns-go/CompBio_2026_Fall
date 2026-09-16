2562006fd5f5b5f00a59cc982d9998545289a4c0
KBurns-go <mknburns@gmail.com>
2026-09-16 18:47:25 -0400

    Add about_me.py via file upload

---
+++ Practicals/W4/about_me.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Declaring variables as strings, listed in full with prompt, for each about me category
+
+name = "My name: Kenzie"
+color = "My favorite color: Yellow"
+activity = "My favorite activity: Hiking"
+animal = "My favorite animals: Frogs and Beluga whales"
+
+# Using print command so code will type out variable information in list format
+# when run on the terminal. 
+
+# I'm using sep='\n' to tell the print command to start a new line after each input variable
+print(name, color, activity, animal, sep='\n')
